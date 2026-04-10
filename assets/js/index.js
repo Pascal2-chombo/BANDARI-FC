d2f866491382db9188c94cb3e47b0c4ef669d3b4
Pascal006 <chombopascal5@gmail.com>
2026-04-10 17:39:08 +0300

    Website Update

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// <!-- Its used to callback the icons in order for them to appear in the frontend -->
+lucide.createIcons();
+
+// To show and hide the password
+const eyeicon = document.getElementById("eyeicon");
+const password = document.getElementById("password");
+
+eyeicon.onclick = function () {
+  if (password.type == "password") {
+    password.type = "text";
+    eyeicon.src = "assets/images/eye-open.png"
+  }
+  else{
+    password.type = "password"
+    eyeicon.src = "assets/images/eye-close.png"
+  }
+};
